043a90991af261531f2b920a9e48fbce432f9f36
link01153113 <link01153113@gmail.com>
2021-09-23 01:08:12 -0400

    Add choose date feature and fix the problem of choosing the date that was in the past

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,9 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   fontSize: 18,
                 ),
+                button: TextStyle(
+                  color: Colors.white,
+                ),
               ),
           appBarTheme: AppBarTheme(
             textTheme: ThemeData.light().textTheme.copyWith(
@@ -69,11 +72,12 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  void _addNewTranscation(String txTitle, double txAmount) {
+  void _addNewTranscation(
+      String txTitle, double txAmount, DateTime chosenDate) {
     final newTx = Transaction(
       title: txTitle,
       amount: txAmount,
-      date: DateTime.now(),
+      date: chosenDate,
       id: DateTime.now().toString(),
     );
 
